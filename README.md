8e6ed74da64f57be36be83b8e196263c46c09d6a
Jared McAteer <jared@arterys.com>
2016-06-02 12:14:52 -0600

    Creating polyfill for TypedArrays in Node v0.12

---
+++ README.md
@@ -0,0 +1,38 @@
+# TypedArray Slice Polyfill
+
+Node v0.12 changed from a native implementation of TypedArrays to the V8 engine
+implementation. Unfortunately during this time the `slice` method on the
+TypedArrays had not been implemented yet. There is another method that is
+similar, `subarray`, however, it creates a live view of the array rather than
+a shallow copy, thus editing either the original array or the subarray would
+have effects on each other.
+
+The purpose of this polyfill to restore the shallow copy functionality in 
+TypedArrays for v0.12. v0.10 and v4+ all contain a functional `slice` method
+implementation. 
+
+## Installation
+
+`npm install typedarray-slice`
+
+## Usage
+
+As this is a polyfill it does not need export anything, simply require the
+module at the beginning of your app and all TypedArrays will have the `slice`
+method added to their prototype
+
+e.g.,
+
+```
+// index.js
+require('typedarray-slice');
+```
+
+## Notes
+
+This polyfill is mostly based on the algorithm provided by [The MDN article on
+polyfilling old browsers to support `slice` in DOM lists][1], however, it has been
+adapted for use in TypedArrays and these arrays will be of the same type as the
+original array they were sliced from.
+
+[1]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice#Streamlining_cross-browser_behavior
---
+++ test/typedarray-slice-test.js
@@ -0,0 +1,110 @@
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('chai-fuzzy'));
+
+describe('TypedArray', function () {
+    if (process.version.indexOf('v0.12') > -1) {
+        describe('without the polyfill', function () {
+            it('should not be able to slice on v0.12', function () {
+                var arr = new Int8Array(new ArrayBuffer(9));
+                expect(arr.hasOwnProperty('slice')).to.be.false;
+            });
+        });
+    } else {
+        console.warning(
+            'This test is being run on node ' + process.version +
+            ' which has native slice support for TypedArrays.'
+        );
+    }
+
+    describe('after polyfill is applied', function () {
+        require('../src/typedarray-slice');
+        var typedArrays = [
+            Int8Array,
+            Uint8Array,
+            Int16Array,
+            Uint16Array,
+            Int32Array,
+            Uint32Array,
+            Float32Array,
+            Float64Array
+        ];
+
+        var arrs = [];
+
+        typedArrays.forEach(function (typedArray) {
+            var tArray = new typedArray(new ArrayBuffer(32));
+            var vals = [];
+            for (var i = 0; i < 32 / typedArray.BYTES_PER_ELEMENT; i++) {
+                vals.push(Math.round(Math.random() * 16));
+            }
+
+            tArray.set(vals);
+            arrs.push(tArray);
+        });
+
+        arrs.forEach(function (arr) {
+            describe(arr.constructor.name, function () {
+                it('should have the slice method', function () {
+                    expect(arr).to.have.property('slice'); 
+                });
+
+                it('should create a shallow copy when slicing', function () {
+                    var copy = arr.slice(0);
+
+                    expect(copy).to.be.a(arr.constructor.name);
+                    expect(copy.length).to.equal(arr.length);
+                    expect(copy.byteLength).to.equal(arr.byteLength);
+                    expect(copy).to.be.like(arr);
+
+                    // Unlike subarray slice should not be a live view of the 
+                    // buffer and should not update the original copy when 
+                    // changed.
+                    copy[0] = copy[0] + 2;
+                    expect(copy).to.not.be.like(arr);
+                });
+
+                it('should slice a subset of the array', function () {
+                    var copy = arr.slice(0, 2);
+
+                    expect(copy.length).to.equal(2);
+                    expect(copy[0]).to.be.equal(arr[0]);
+                    expect(copy[1]).to.be.equal(arr[1]);
+                });
+
+                it('should slice with a negative start', function () {
+                    var copy = arr.slice(-2);
+
+                    expect(copy.length).to.equal(2);
+                    expect(copy[0]).to.be.equal(arr[arr.length-2]);
+                    expect(copy[1]).to.be.equal(arr[arr.length-1]);
+                });
+
+                it('should slice with a negative end', function () {
+                    var copy = arr.slice(-2, -1);
+
+                    expect(copy.length).to.equal(1);
+                    expect(copy[0]).to.be.equal(arr[arr.length-2]);
+                });
+
+                it('should be empty when given a positive start greater than the lenght', function () {
+                    var copy = arr.slice(arr.length + 2);
+                    expect(copy).to.be.empty;
+                });
+
+                it('should be empty if given a negative start less than the positive end', function () {
+                    var copy = arr.slice(-2, 1);
+                    expect(copy).to.be.empty;
+                });
+
+                it('should be wrap negative starts when greater than the posititve', function () {
+                    var copy = arr.slice((-1 * (arr.length - 1)), 3);
+
+                    expect(copy.length).to.equal(2);
+                    expect(copy[0]).to.equal(arr[1]);
+                    expect(copy[1]).to.equal(arr[2]);
+                });
+            });
+        });
+    });
+});
